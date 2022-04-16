e9b7873190714a00cb836ef588cd8e07286927f6
junepro <57428866+junepro@users.noreply.github.com>
2022-04-16 11:56:42 +0900

    restTemplate 오류 또남

--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 public interface UserService extends UserDetailsService {
+
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
